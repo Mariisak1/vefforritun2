1e7b76673a997e58b25b2ec84727a91fffcaf448
Sakkinn <isakmar00@gmail.com>
2023-02-26 12:51:34 +0000

    nýskráning útfærð

---
+++ v2/src/routes/create-user.js
@@ -0,0 +1,4 @@
+import { createUser } from '../lib/users.js';
+
+createUser("notandi", "123")
+
--- v2/src/routes/user-routes.js
+++ v2/src/routes/user-routes.js
@@ -16,13 +16,18 @@ import {
   xssSanitizationMiddleware,
 } from '../lib/validation.js';
 
+import { createUser } from '../lib/users.js';
+
 export const userRouter = express.Router();
 
 async function userRoute(req, res) {
   const events = await listEvents();
+  const { user: { username } = {} } = req || {};
 
-  res.render('index', {
-    title: 'Viðburðasíðan',
+  return res.render('user', {
+    username,
+    errors: [],
+    title: 'Viðburðir — notandi',
     admin: false,
     events,
   });
@@ -45,9 +50,39 @@ function login(req, res) {
     return res.render('loginUser', { message, title: 'Innskráning' });
 }
 
+function signup(req, res) {
+
+  console.log(req);
+
+  //TODO IF USER DOES NOT EXIST THEN CALL "createUser"
+  if (req.isAuthenticated()) {
+    return res.redirect('/user');
+  }
+
+
+
+  let message = '';
+
+  // Athugum hvort einhver skilaboð séu til í session, ef svo er birtum þau
+  // og hreinsum skilaboð
+  if (req.session.messages && req.session.messages.length > 0) {
+    message = req.session.messages.join(', ');
+    req.session.messages = [];
+  }
+
+  return res.render('signup-user', { message, title: 'Nýskráning' });
+}
+
+
+userRouter.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
 
 userRouter.get('/', ensureLoggedInUser, catchErrors(userRoute));
 userRouter.get('/login', login);
+userRouter.get('/signup', signup);
+
 userRouter.post(
   '/login',
 
@@ -61,4 +96,27 @@ userRouter.post(
   (req, res) => {
     res.redirect('/user');
   }
+);
+
+userRouter.post(
+  '/signup', 
+  
+  async (req, res) => {
+
+  const { username, password } = req.body;
+
+  try {
+    await createUser(username, password);
+
+    //Skráir notanda sjálfkrafa inn eftir sign-up
+    passport.authenticate('local')(req, res, () => {
+      res.redirect('/user');
+    });
+  } 
+  
+  catch(error) {
+    return res.render('user/signup')
+  }
+
+}
 );

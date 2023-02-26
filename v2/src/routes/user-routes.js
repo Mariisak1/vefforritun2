30845c3c032fde6ec3dfe4ed67009acb8712820b
Sakkinn <isakmar00@gmail.com>
2023-02-26 00:03:41 +0000

    fix

---
+++ v2/src/routes/user-routes.js
@@ -0,0 +1,64 @@
+import express from 'express';
+import { validationResult } from 'express-validator';
+import { catchErrors } from '../lib/catch-errors.js';
+import {
+  createEvent,
+  listEvent,
+  listEventByName,
+  listEvents,
+  updateEvent,
+} from '../lib/db.js';
+import passport, { ensureLoggedInUser } from '../lib/login.js';
+import { slugify } from '../lib/slugify.js';
+import {
+  registrationValidationMiddleware,
+  sanitizationMiddleware,
+  xssSanitizationMiddleware,
+} from '../lib/validation.js';
+
+export const userRouter = express.Router();
+
+async function userRoute(req, res) {
+  const events = await listEvents();
+
+  res.render('index', {
+    title: 'Viðburðasíðan',
+    admin: false,
+    events,
+  });
+}
+
+function login(req, res) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/user');
+    }
+  
+    let message = '';
+  
+    // Athugum hvort einhver skilaboð séu til í session, ef svo er birtum þau
+    // og hreinsum skilaboð
+    if (req.session.messages && req.session.messages.length > 0) {
+      message = req.session.messages.join(', ');
+      req.session.messages = [];
+    }
+  
+    return res.render('loginUser', { message, title: 'Innskráning' });
+}
+
+
+userRouter.get('/', ensureLoggedInUser, catchErrors(userRoute));
+userRouter.get('/login', login);
+userRouter.post(
+  '/login',
+
+  // Þetta notar strat að ofan til að skrá notanda inn
+  passport.authenticate('local', {
+    failureMessage: 'Notandanafn eða lykilorð vitlaust.',
+    failureRedirect: '/user/login',
+  }),
+
+  // Ef við komumst hingað var notandi skráður inn, senda á /user
+  (req, res) => {
+    res.redirect('/user');
+  }
+);

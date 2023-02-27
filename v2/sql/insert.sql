238526370b2fb5af9f309114f027f708fedb1d4c
Sakkinn <isakmar00@gmail.com>
2023-02-27 20:38:13 +0000

    fix insert.sql

--- v2/sql/insert.sql
+++ v2/sql/insert.sql
@@ -2,4 +2,4 @@ INSERT INTO events (id, name, slug, description, location, path_url) VALUES (1, 
 INSERT INTO events (id, name, slug, description, location, path_url) VALUES (2, 'Hönnuðahittingur í mars', 'honnudahittingur-i-mars', 'Spennandi hittingur hönnuða í Hönnunarmars.', 'Nesvegur 23, Hvolsvelli', 'waigjaog.is');
 INSERT INTO events (id, name, slug, description, location, path_url) VALUES (3, 'Verkefnastjórahittingur í apríl', 'verkefnastjorahittingur-i-april', 'Virkilega vel verkefnastýrður hittingur.', 'Álfaskeið 57, Hafnarfirði', null);
 
-INSERT INTO users (username, password) VALUES ('admin', '$2a$11$pgj3.zySyFOvIQEpD7W6Aund1Tw.BFarXxgLJxLbrzIv/4Nteisii');
+INSERT INTO users (username, password, isadmin) VALUES ('admin', '$2a$11$pgj3.zySyFOvIQEpD7W6Aund1Tw.BFarXxgLJxLbrzIv/4Nteisii', 'true');

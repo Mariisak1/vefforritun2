dbdba73910bdc8a1a42586fc69dfec605ef999cd
Sakkinn <isakmar00@gmail.com>
2023-02-27 19:53:33 +0000

    setup.js fix

--- v2/sql/insert.sql
+++ v2/sql/insert.sql
@@ -1,9 +1,5 @@
-INSERT INTO events (id, name, slug, description) VALUES (1, 'Forritarahittingur í febrúar', 'forritarahittingur-i-februar', 'Forritarar hittast í febrúar og forrita saman eitthvað frábært.');
-INSERT INTO events (id, name, slug, description) VALUES (2, 'Hönnuðahittingur í mars', 'honnudahittingur-i-mars', 'Spennandi hittingur hönnuða í Hönnunarmars.');
-INSERT INTO events (id, name, slug, description) VALUES (3, 'Verkefnastjórahittingur í apríl', 'verkefnastjorahittingur-i-april', 'Virkilega vel verkefnastýrður hittingur.');
-
-INSERT INTO registrations (name, comment, event) VALUES ('Forvitinn forritari', 'Hlakka til að forrita með ykkur', 1);
-INSERT INTO registrations (name, comment, event) VALUES ('Jón Jónsson', null, 1);
-INSERT INTO registrations (name, comment, event) VALUES ('Guðrún Guðrúnar', 'verður vefforritað?', 1);
+INSERT INTO events (id, name, slug, description, location, path_url) VALUES (1, 'Forritarahittingur í febrúar', 'forritarahittingur-i-februar', 'Forritarar hittast í febrúar og forrita saman eitthvað frábært.', 'Hlíðarendi 35, Fosshóli', null);
+INSERT INTO events (id, name, slug, description, location, path_url) VALUES (2, 'Hönnuðahittingur í mars', 'honnudahittingur-i-mars', 'Spennandi hittingur hönnuða í Hönnunarmars.', 'Nesvegur 23, Hvolsvelli', 'waigjaog.is');
+INSERT INTO events (id, name, slug, description, location, path_url) VALUES (3, 'Verkefnastjórahittingur í apríl', 'verkefnastjorahittingur-i-april', 'Virkilega vel verkefnastýrður hittingur.', 'Álfaskeið 57, Hafnarfirði', null);
 
 INSERT INTO users (username, password) VALUES ('admin', '$2a$11$pgj3.zySyFOvIQEpD7W6Aund1Tw.BFarXxgLJxLbrzIv/4Nteisii');
